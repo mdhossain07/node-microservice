53d7c290df33fbb6b6ea70dce6aa239bc881d021
mdhossain07 <mrabir8097@gmail.com>
2025-11-26 10:20:16 +0600

    chore: Remove debug log for MongoDB URI connection.

--- user-service/config/db.js
+++ user-service/config/db.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default async function connectDB() {
-  console.log("hitting this", process.env.MONGODB_URI);
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected");
